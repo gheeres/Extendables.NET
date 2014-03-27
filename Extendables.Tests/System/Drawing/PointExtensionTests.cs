adc0db3744e0d9fdeb860a676afe4ce2495c1fd5
George H <gheeres@uwsp.edu>
2014-03-27 10:53:13 -0500

    Add extension methods and associated tests for System.Drawing.Point to calculate bounding rectangles, distances (X, Y and Euclidean).

---
+++ Extendables.Tests/System/Drawing/PointExtensionTests.cs
@@ -0,0 +1,191 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+
+namespace Extendables.Tests.System.Drawing
+{
+  [TestFixture]
+  class PointExtensionTests
+  {
+    [SetUp]
+    public void Initialize()
+    {
+    }
+
+    [Test]
+    public void DistanceXWithEmptyStartPointReturns0()
+    {
+      Point start = Point.Empty;
+      Point end = new Point(10, 10);
+
+      Assert.AreEqual(0, start.DistanceX(end));
+    }
+
+    [Test]
+    public void DistanceXWithEmptyEndPointReturns0()
+    {
+      Point start = new Point(10, 10);
+      Point end = Point.Empty;
+
+      Assert.AreEqual(0, start.DistanceX(end));
+    }
+
+    [Test]
+    public void DistanceXWithEqualStartXAndEndXPointReturns0()
+    {
+      Point start = new Point(1, 10);
+      Point end = new Point(1, 20);
+
+      Assert.AreEqual(0, start.DistanceX(end));
+    }
+
+    [Test]
+    public void DistanceXWithStartXLessThanEndXReturnsNegativeValue()
+    {
+      Point start = new Point(10, 10);
+      Point end = new Point(1, 20);
+
+      Assert.AreEqual(-9, start.DistanceX(end));
+    }
+
+    [Test]
+    public void DistanceXWithStartXGreaterThanEndXReturnsPositiveValue()
+    {
+      Point start = new Point(1, 10);
+      Point end = new Point(10, 20);
+
+      Assert.AreEqual(9, start.DistanceX(end));
+    }
+
+    [Test]
+    public void DistanceYWithEmptyStartPointReturns0()
+    {
+      Point start = Point.Empty;
+      Point end = new Point(10, 10);
+
+      Assert.AreEqual(0, start.DistanceY(end));
+    }
+
+    [Test]
+    public void DistanceYWithEmptyEndPointReturns0()
+    {
+      Point start = new Point(10, 10);
+      Point end = Point.Empty;
+
+      Assert.AreEqual(0, start.DistanceY(end));
+    }
+
+    [Test]
+    public void DistanceYWithEqualStartXAndEndXPointReturns0()
+    {
+      Point start = new Point(1, 10);
+      Point end = new Point(10, 10);
+
+      Assert.AreEqual(0, start.DistanceY(end));
+    }
+
+    [Test]
+    public void DistanceYWithStartXLessThanEndXReturnsNegativeValue()
+    {
+      Point start = new Point(10, 40);
+      Point end = new Point(1, 20);
+
+      Assert.AreEqual(-20, start.DistanceY(end));
+    }
+
+    [Test]
+    public void DistanceYWithStartXGreaterThanEndXReturnsPositiveValue()
+    {
+      Point start = new Point(1, 10);
+      Point end = new Point(10, 20);
+
+      Assert.AreEqual(10, start.DistanceY(end));
+    }
+
+    [Test]
+    public void GetBoundingRectangleWithEmptyStartReturnsEmptyRectangle()
+    {
+      Point start = Point.Empty;
+      Point end = new Point(10, 20);
+
+      Assert.AreEqual(Rectangle.Empty, start.GetBoundingRectangle(end));
+    }
+
+    [Test]
+    public void GetBoundingRectangleWithEmptyEndReturnsEmptyRectangle()
+    {
+      Point start = new Point(10, 20);
+      Point end = Point.Empty;
+
+      Assert.AreEqual(Rectangle.Empty, start.GetBoundingRectangle(end));
+    }
+
+    [Test]
+    public void GetBoundingRectangleWithEqualPointReturnsRectangleWithNoWidthOrHeight()
+    {
+      Point start = new Point(10, 20);
+      Point end = new Point(10, 20);
+      Rectangle expected = new Rectangle(10, 20, 0, 0);
+
+      Assert.AreEqual(expected, start.GetBoundingRectangle(end));
+    }
+
+    [Test]
+    public void GetBoundingRectangleWithStartXLessThenEndXReturnsAdjustedRectangle()
+    {
+      Point start = new Point(10, 20);
+      Point end = new Point(5, 25);
+      Rectangle expected = new Rectangle(5, 20, 5, 5);
+
+      Assert.AreEqual(expected, start.GetBoundingRectangle(end));
+    }
+
+    [Test]
+    public void GetBoundingRectangleWithStartYLessThenEndYReturnsAdjustedRectangle()
+    {
+      Point start = new Point(10, 20);
+      Point end = new Point(15, 10);
+      Rectangle expected = new Rectangle(10, 10, 5, 10);
+
+      Assert.AreEqual(expected, start.GetBoundingRectangle(end));
+    }
+
+    [Test]
+    public void DistanceWithEmptyStartReturns0()
+    {
+      Point start = Point.Empty;
+      Point end = new Point(10, 20);
+
+      Assert.AreEqual(0, start.Distance(end));
+    }
+
+    [Test]
+    public void DistanceWithEmptyEndReturns0()
+    {
+      Point start = new Point(10, 20);
+      Point end = Point.Empty;
+
+      Assert.AreEqual(0, start.Distance(end));
+    }
+
+    [Test]
+    public void DistanceWithEqualPointsReturns0()
+    {
+      Point start = new Point(10, 20);
+      Point end = new Point(10, 20);
+
+      Assert.AreEqual(0, start.Distance(end));
+    }
+
+    [Test]
+    public void DistanceReturnsEuclideanDistance()
+    {
+      // Using a standard 3 / 4 right angle triangle.
+      // To give us a nice even return value of 5
+      // a^2 + b^2 = c^2 => 9 + 16 = 25
+      Point start = new Point(10, 3);
+      Point end = new Point(14, 6);
+
+      Assert.AreEqual(5, start.Distance(end));
+    }
+  }
+}
---
+++ Extendables/System/Drawing/PointExtensions.cs
@@ -0,0 +1,60 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Drawing
+{
+  /// <summary>
+  /// Extension methods for the <see cref="Point"/> class.
+  /// </summary>
+  public static class PointExtensions
+  {
+    /// <summary>
+    /// Calculates the X distance between the two points. The value returned can be positive or negative.
+    /// </summary>
+    /// <param name="start">The starting reference Point.</param>
+    /// <param name="end">The distance being calculated.</param>
+    /// <returns>The positive or negative distance in the the X coordinate space.</returns>
+    public static int DistanceX(this Point start, Point end)
+    {
+      if ((start.IsEmpty) || (end.IsEmpty)) return (0);
+      return (end.X - start.X);
+    }
+
+    /// <summary>
+    /// Calculates the Y distance between the two points. The value returned can be positive or negative.
+    /// </summary>
+    /// <param name="start">The starting reference Point.</param>
+    /// <param name="end">The distance being calculated.</param>
+    /// <returns>The positive or negative distance in the the Y coordinate space.</returns>
+    public static int DistanceY(this Point start, Point end)
+    {
+      if ((start.IsEmpty) || (end.IsEmpty)) return (0);
+      return (end.Y - start.Y);
+    }
+
+    /// <summary>
+    /// Calculates the eclidean distance between the two points.
+    /// </summary>
+    /// <param name="start">The starting reference Point.</param>
+    /// <param name="end">The distance being calculated.</param>
+    /// <returns>The distance between the two points.</returns>
+    public static double Distance(this Point start, Point end)
+    {
+      if ((start == Point.Empty) || (end == Point.Empty)) return (0);
+      return(Math.Sqrt(Math.Pow(start.X - end.X, 2) + Math.Pow(start.Y - end.Y, 2)));
+    }
+
+    /// <summary>
+    /// Get's the bounding rectangle for the two points. 
+    /// </summary>
+    /// <param name="start">The starting reference point.</param>
+    /// <param name="end">The ending reference point.</param>
+    /// <returns>The bounding rectangle for the points. If either point is <see cref="Point.Empty"/> then a <see cref="Rectangle.Empty"/> is returned.</returns>
+    public static Rectangle GetBoundingRectangle(this Point start, Point end)
+    {
+      if ((start == Point.Empty) || (end == Point.Empty)) return (Rectangle.Empty);
+      return (new Rectangle(Math.Min(start.X, end.X),
+                            Math.Min(start.Y, end.Y),
+                            Math.Abs(end.X - start.X), 
+                            Math.Abs(end.Y - start.Y)));
+    }
+  }
+}
